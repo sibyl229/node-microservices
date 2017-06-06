c291c3b5d0d41a7465cb65f7e5bbd6f85ae6bba9
Sibyl <sibyl@wormbase.org>
2017-06-05 22:46:28 -0400

    control connected tab with route

--- client/src/app/ConnectedHeader.js
+++ client/src/app/ConnectedHeader.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'material-ui/Tabs';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
-import Header from '../components/layout/Header';
 
 const mapStateToProps = (state) => {
-  return {};
+  return {
+    value: state.router.location.pathname
+  };
 };
 
-export default connect(mapStateToProps)(Header);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onChange: (tabValue) => {
+      dispatch(push(tabValue))
+    }
+  };
+};
+
+const ConnectedTabs = connect(mapStateToProps, mapDispatchToProps)(Tabs);
+
+export default (props) => (
+  <ConnectedTabs>
+    <Tab label='A' value='/A'>
+      A
+    </Tab>
+    <Tab label='B' value='/B'>
+      B
+    </Tab>
+  </ConnectedTabs>
+)
--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,13 +1,1 @@
-import React, { Component } from 'react';
-import { Tabs, Tab } from 'material-ui/Tabs';
 
-export default class Header extends Component {
-  render() {
-    return(
-      <Tabs>
-        <Tab label="A">
-        </Tab>
-      </Tabs>
-    )
-  }
-}
