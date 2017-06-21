90339a71021a37fa96679fadeb96bab1701a01ad
Sibyl <sibyl@wormbase.org>
2017-06-20 22:37:30 -0400

    move name field to top

--- client/src/content/widgets/GeneExpressionWidget.js
+++ client/src/content/widgets/GeneExpressionWidget.js
@@ -6,16 +6,25 @@ import ControlledTable from '../../components/elements/ControlledTable';
 const GeneExpressionWidget = (props) => {
   return (
     <div>
-      {JSON.stringify(Object.keys(props.data.fields), null, 4)}
+      {/* {JSON.stringify(Object.keys(props.data.fields), null, 4)} */}
 
-      <br/>custom expression widget
+      <p>start of custom fields</p>
+      <h3>{props.data.fields.name.data.label}</h3>
+
+      <FieldWrapper
+        title="WormBase ID"
+        data={props.data.fields.name}
+        render={({data}) => <div>{data.id}</div>}/>
+
       <FieldWrapper
         title={'Expression cluster'}
         data={props.data.fields.expression_cluster}
         render={ControlledTable} />
+      <p>end of custom fields</p>
+
       {
         Object.keys(props.data.fields).filter(
-          (fieldKey) => fieldKey !== 'expression_cluster'
+          (fieldKey) => fieldKey !== 'expression_cluster' && fieldKey !== 'name'
         ).map((fieldKey) => (
           <FieldWrapper key={fieldKey}
             title={fieldKey.replace(/_+/g, ' ')}
--- client/src/content/widgets/GenericWidget.js
+++ client/src/content/widgets/GenericWidget.js
@@ -5,9 +5,18 @@ import FieldWrapper from '../../components/elements/FieldWrapper';
 const GenericWidget = (props) => {
   return (
     <div>
-      {JSON.stringify(Object.keys(props.data.fields), null, 4)}
+      {/* {JSON.stringify(Object.keys(props.data.fields), null, 4)} */}
       {
-        Object.keys(props.data.fields).map((fieldKey) => (
+        Object.keys(props.data.fields).sort(
+          (fieldKeyA, fieldKeyB) => {
+            const getPriority = (fieldKey) => {
+              const priorityArray = ["name"];
+              const index = priorityArray.indexOf(fieldKey);
+              return index === -1 ? priorityArray.length : index;
+            }
+            return getPriority(fieldKeyA) - getPriority(fieldKeyB);
+          }
+        ).map((fieldKey) => (
           <FieldWrapper key={fieldKey}
             title={fieldKey.replace(/_+/g, ' ')}
             data={props.data.fields[fieldKey]} />
